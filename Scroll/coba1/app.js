4600f3d590beeb97f8401fdec0198f6248a7dd6c
aliffiansyah <mochammadaliffiansyah@gmail.com>
2022-01-29 11:13:51 +0700

    menambahkan toggleactions

--- Scroll/coba1/app.js
+++ Scroll/coba1/app.js
@@ -6,8 +6,14 @@ gsap.to(".square",{
     scrollTrigger: {
         trigger : ".square",
         start : "top 50%",
-        end : ()=> `+=${document.querySelector(".square").offsetHeight}`,
-        markers : true,
+        // end : ()=> `+=${document.querySelector(".square").offsetHeight}`,
+        end : "center 20%",
+        toggleActions: "restart pause resume complete",
+        //              play pause reverve restart reset complete none
+        //              onEnter onLeave OnEnterBack onLeaveBack
+        markers : {
+            fontSize :"3rem",
+        },
         toggleClass : "red"
     }
 })
